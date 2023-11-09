a151c4b1fa3ea754cdf5ac36d88a359d56d09948
Wang, Charlie <cywang3@illinois.edu>
2023-11-09 16:12:01 -0600

    Added basic search bar on UI

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config import API_KEY
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home_page():
-    return render_template("index.html", api_key=API_KEY) 
+    if request.method == 'POST':
+        search_query = request.form['searchQuery']
+        # Here you would typically process the search_query.
+        # For now, we're just passing it back to the template.
+        return render_template("index.html", api_key=API_KEY, search_query=search_query)
+    return render_template("index.html", api_key=API_KEY)
+
+if __name__ == "__main__":
+    app.run(debug=True)
