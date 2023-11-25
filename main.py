f73477534f32b7a5e65093bc06b4cd6810f1abaf
Devak Nanda <devaknanda@hotmail.com>
2023-11-25 13:19:23 -0600

    map functionality in vue 🗺️⚙️

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ API_KEY = os.environ.get('API_KEY')
 ENDPOINT = os.environ.get('COSMOS_ENDPOINT')
 KEY = os.environ["COSMOS_KEY"]
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 db_client = CosmosClient(url=ENDPOINT, credential=KEY)
 
 app.jinja_env.variable_start_string = '[['
 app.jinja_env.variable_end_string = ']]'
 
 
-@app.route("/search", methods=['GET'])
-def search():
-    query = request.args.get('query')
-    print(f'user searched for: {query}')
-    return render_template("search.html", api_key=API_KEY, query=query)
+# @app.route("/search", methods=['GET'])
+# def search():
+#     query = request.args.get('query')
+#     print(f'user searched for: {query}')
+#     return render_template("search.html", api_key=API_KEY, query=query)
 
 # @app.route("/search/<input>", methods=['GET'])
 # def search
@@ -25,14 +25,7 @@ def search():
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
     print("home page reached")
-    if request.method == 'POST':
-        query = request.form.get('searchQuery', '')
-        # Here you would typically process the search_query.
-        # For now, we're just passing it back to the template.
-
-        return redirect(url_for('search', query=query))
-
-        # return render_template("index.html", api_key=API_KEY, search_query=search_query)
+    # return render_template("index.html", api_key=API_KEY, search_query=search_query)
     return render_template("index.html", api_key=API_KEY)
 
 # @app.route(".")
